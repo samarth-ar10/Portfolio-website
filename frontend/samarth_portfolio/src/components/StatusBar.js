89aee1c4103971f4b51a1d0a93fa80c10a9d2e8f
samarth-ar10 <samarth.ar10@gmail.com>
2024-12-02 21:04:41 +0000

    added github and linkedIn links before uploading to the main server

--- frontend/samarth_portfolio/src/components/StatusBar.js
+++ frontend/samarth_portfolio/src/components/StatusBar.js
@@ -2,21 +2,22 @@
 
 import React from 'react';
 import './StatusBar.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import config from './config';
 
 function StatusBar({ isMenuOpen, handleMenuToggle }) {
     return (
         <header className={`status-bar ${isMenuOpen ? 'menu-open' : ''}`}>
+            <h1 className="app-name">Samarth</h1>
             <div className="social-icons">
-                <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-github"></i>
+                <a href="https://github.com/samarth-ar10" target="_blank" rel="noopener noreferrer">
+                    <FaGithub size={24} />
                 </a>
-                <a href="https://www.linkedin.com/in/your-username/" target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-linkedin"></i>
+                <a href="https://www.linkedin.com/in/samarth-nln/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin size={24} />
                 </a>
             </div>
-            <h1 className="app-name">Samarth</h1>
+
         </header>
     );
 }
