421f9f82d5e94f85b5342184d57a537bd713202d
samarth-ar10 <samarth.ar10@gmail.com>
2024-12-05 02:17:11 +0000

    adding logging function to status bar and removing width from media content

--- frontend/samarth_portfolio/src/components/StatusBar.js
+++ frontend/samarth_portfolio/src/components/StatusBar.js
@@ -5,19 +5,49 @@ import './StatusBar.css';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import config from './config';
 
+const logEventToServer = async (eventType, data) => {
+    try {
+        const response = await fetch(config.URL + '/api/frontend_log', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ event_type: eventType, data }),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+    } catch (error) {
+        console.error('Error logging event to server:', error);
+    }
+};
+
 function StatusBar({ isMenuOpen, handleMenuToggle }) {
+    const handleLinkClick = (platform) => {
+        logEventToServer('social_link_click', { platform });
+    };
+
     return (
         <header className={`status-bar ${isMenuOpen ? 'menu-open' : ''}`}>
             <h1 className="app-name">Samarth</h1>
             <div className="social-icons">
-                <a href="https://github.com/samarth-ar10" target="_blank" rel="noopener noreferrer">
+                <a
+                    href="https://github.com/samarth-ar10"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={() => handleLinkClick('GitHub')}
+                >
                     <FaGithub size={24} />
                 </a>
-                <a href="https://www.linkedin.com/in/samarth-nln/" target="_blank" rel="noopener noreferrer">
+                <a
+                    href="https://www.linkedin.com/in/samarth-nln/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={() => handleLinkClick('LinkedIn')}
+                >
                     <FaLinkedin size={24} />
                 </a>
             </div>
-
         </header>
     );
 }
